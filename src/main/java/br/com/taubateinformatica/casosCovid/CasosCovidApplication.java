d49ab9bdf7d0b346aa50dc98cb627bff85c2c72e
Tathiane Lima <tathiane.lima@hotmail.com>
2020-10-27 14:02:16 -0300

    Projeto Criado

---
+++ src/main/java/br/com/taubateinformatica/casosCovid/CasosCovidApplication.java
@@ -0,0 +1,13 @@
+package br.com.taubateinformatica.casosCovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasosCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasosCovidApplication.class, args);
+	}
+
+}
