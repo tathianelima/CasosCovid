d2e494dc1d2516753655bad09e9f84d2251731e6
Tathiane Lima <tathiane.lima@hotmail.com>
2020-10-28 01:12:07 -0300

    Criada a classe de domínio CasosPorMes

---
+++ src/main/java/br/com/taubateinformatica/casosCovid/domain/CasosPorMes.java
@@ -0,0 +1,47 @@
+package br.com.taubateinformatica.casosCovid.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class CasosPorMes {
+
+    public class CasosPorMes implements Serializable {
+        private static final long serialVersionUID = 1L;
+
+        @Id
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
+        private Integer id;
+        private String mes;
+        private Integer casos;
+
+        public CasosPorMes(Integer id, String mes, Integer casos) {
+            this.id = id;
+            this.mes = mes;
+            this.casos = casos;
+        }
+
+        public String getMes() {
+            return mes;
+        }
+
+        public void setMes(String mes) {
+            this.mes = mes;
+        }
+
+        public Integer getCasos() {
+            return casos;
+        }
+
+        public void setCasos(Integer casos) {
+            this.casos = casos;
+        }
+    }
+
+
+}
